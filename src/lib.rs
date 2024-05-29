4606ae66dd4c577060091e179ac6cd245657fe57
Vishal <vishal@Aizadas-MBP.fios-router.home>
2024-05-29 12:23:29 -0400

    initial subscribe method

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use axum::{extract::Query, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::Query,
+    http::StatusCode,
+    routing::{get, post},
+    Form, Router,
+};
 use serde::Deserialize;
 use tokio::net::TcpListener;
 
@@ -7,9 +12,17 @@ struct GreetQuery {
     name: Option<String>,
 }
 
+#[derive(Debug, Deserialize)]
+struct SubscriberInfo {
+    name: String,
+    email: String,
+}
+
 async fn health_check() -> (StatusCode, &'static str) {
     (StatusCode::OK, "Ok")
 }
+
+async fn subscribe(Form(subscriber_info): Form<SubscriberInfo>) {}
 async fn greet(Query(query): Query<GreetQuery>) -> String {
     let query = query.name.unwrap_or_else(|| "World".into());
     format!("Hello {}", query)
@@ -18,7 +31,8 @@ async fn greet(Query(query): Query<GreetQuery>) -> String {
 pub async fn run(listener: TcpListener) {
     let app = Router::new()
         .route("/", get(greet))
-        .route("/health_check", get(health_check));
+        .route("/health_check", get(health_check))
+        .route("/subscribe", post(subscribe));
 
     axum::serve(listener, app).await.unwrap();
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,18 @@
+use axum::http::HeaderName;
+use reqwest::Client;
 use tokio::sync::oneshot;
 use zero2prod::run;
+
+fn spawn_app() -> oneshot::Receiver<u16> {
+    let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:0");
+    let (tx, rw) = oneshot::channel();
+    let _ = tokio::spawn(async move {
+        let tcp_listener = tcp_listener.await.unwrap();
+        let _ = tx.send(tcp_listener.local_addr().unwrap().port());
+        run(tcp_listener).await;
+    });
+    rw
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -19,13 +32,46 @@ async fn health_check_works() {
     assert_eq!(Some(2), response.content_length());
 }
 
-fn spawn_app() -> oneshot::Receiver<u16> {
-    let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:0");
-    let (tx, rw) = oneshot::channel();
-    let _ = tokio::spawn(async move {
-        let tcp_listener = tcp_listener.await.unwrap();
-        let _ = tx.send(tcp_listener.local_addr().unwrap().port());
-        run(tcp_listener).await;
-    });
-    rw
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let port = spawn_app().await.unwrap();
+    let client = Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("http://localhost:{}/subscribe", port))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to send subscribe message");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let port = spawn_app().await.unwrap();
+    let client = Client::new();
+
+    let data = vec![
+        ("name=le%20guin", "missing email"),
+        ("email=ursula_le_guin%40gmail.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+
+    for (body, error_msg) in data {
+        let response = client
+            .post(format!("http://localhost:{}/subscribe", port))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Api did not fail with 400 when payload was {}",
+            error_msg
+        );
+    }
 }
